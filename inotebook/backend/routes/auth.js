36f6a43c26df59831ed3125a1ad3be1dba8c62cb
Yash <yash8164@gmail.com>
2022-08-29 16:11:15 +0530

    Added validations for Users

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const Users = require('../models/Users');
+const express = require("express");
+const Users = require("../models/Users");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
 // Create a User using : POST "/api/auth/". Doesn't require auth
 
-router.post('/', (req, res) => {
-    const user = Users(req.body);
-    console.log(user);
-    user.save();
-    res.json(user);
-});
+router.post(
+  "/",
+  body("email", "Enter valid email-id").isEmail(),
+  body("name", "Enter valid name").isLength({ min: 3 }),
+	body('password', "Password must be 8 characters long").isLength({min: 8}),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    Users.create({
+			name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+		.catch(err => {console.log(err)
+		res.json({"error": "Please enter unique value for email",
+			"message": err.message})});
+  }
+);
 
 module.exports = router;
