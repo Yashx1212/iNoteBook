7faaca9a0b6e3929f965717aaf1a64bb93d20980
Yash <yash8164@gmail.com>
2022-09-05 11:52:48 +0530

    Adding BcryptJS and AuthToken

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const Users = require("../models/Users");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require('bcryptjs');
+const JWT_SIGN = 'Masterkey#1';
+const jwt = require('jsonwebtoken');
 // Create a User using : POST "/api/auth/createuser". Doesn't require auth
 
 router.post(
@@ -24,13 +26,25 @@ router.post(
     if(user){
       res.status(400).json({error: "Sorry a user with this email already exist"});
     }
-
+    const salt = await bcrypt.genSalt(10);
+    const securedPassword = await bcrypt.hash(req.body.password,salt);
+    //Create a new User
     user = await Users.create({
 			name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: securedPassword,
     });
-    res.json(user);
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    };
+
+    const authToken = jwt.sign(data,JWT_SIGN);
+    //console.log(jwtData);
+    res.json({authToken});
+    //res.json(user);
   }
 );
 
